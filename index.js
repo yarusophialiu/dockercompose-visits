c7bc79cf002cbe6c8e7b1fd6581e254ed5f539f7
yaruliu <yarusophia@gmail.com>
2019-10-02 23:23:32 -0400

    update redis create client

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const redis = require('redis')
+const process = require('process')
 
 const app = express()
-const client = redis.createClient()
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
+// 0 is status code: exit and everything is ok
 client.set('visits', 0)
 
 app.get('/', (req, res) => {
+    // process.exit(0)
     client.get('visits', (err, visits) => {
         res.send('number of visits is ' + visits)
         client.set('visits', parseInt(visits) + 1)
